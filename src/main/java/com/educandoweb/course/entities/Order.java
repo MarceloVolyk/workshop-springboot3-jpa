a56e086e29b0f4970b18a062f02bbabc4db340f7
MarceloVolyk <marcelovolyk@gmail.com>
2024-07-17 03:02:26 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -103,6 +103,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		Double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +34,7 @@ public class Payment implements Serializable {
 	private Long id;                      //basic attributes
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;                  //associations(collections)
